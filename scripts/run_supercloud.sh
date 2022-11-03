ffdb508551a309615851d5b278730f7d478b4ed7
Seungwook Han <sh3264@columbia.edu>
2022-11-03 00:32:56 -0400

    added module load

--- scripts/run_supercloud.sh
+++ scripts/run_supercloud.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+module load anaconda/2021a
+
 for lr in 0.05 0.1 0.15 0.2
 do
 python main_simsiam.py --submit --server=sc --arg_str="--batch-size=256 --lr${lr} --world-size 1 --rank 0 --fix-pred-lr"
