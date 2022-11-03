edcabaa95cbf0d982d9ace1f1cc659daf42136c3
Seungwook Han <sh3264@columbia.edu>
2022-11-03 15:36:24 -0400

    fixed arg

--- scripts/run_supercloud_lp.sh
+++ scripts/run_supercloud_lp.sh
@@ -4,5 +4,5 @@ module load anaconda/2021a
 
 for lr in 30 10 1 0.2 0.1
 do
-python main_lincls.py --submit --server=sc --arg_str="--add-prefix $2 --batch-size=256 --lr ${lr} --world-size 1 --rank 0 --pretrained $1"
+python main_lincls.py --submit --server=sc --arg_str="--add_prefix $2 --batch-size=256 --lr ${lr} --world-size 1 --rank 0 --pretrained $1"
 done
